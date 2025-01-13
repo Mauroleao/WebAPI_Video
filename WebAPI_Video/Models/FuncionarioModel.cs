b7fe1ec8df2153c1e43c4f986918a2d7dbb355c6
Mauro <your.email@example.com>
2025-01-13 09:13:18 -0300

    Add project files.

---
+++ WebAPI_Video/Models/FuncionarioModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebAPI_Video.Enums;
+
+namespace WebAPI_Video.Models
+{
+    public class FuncionarioModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Sobrenome { get; set; }
+        public DepartamentoEnum Departamento { get; set; }
+        public bool Ativo { get; set; }
+
+        public  TurnoEnum Turno { get; set; }
+        public DateTime DataDeCriação { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataDeAlteração { get; set; } = DateTime.Now.ToLocalTime();
+
+
+
+
+
+    }
+}
